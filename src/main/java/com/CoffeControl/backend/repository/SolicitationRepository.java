d0fe16ab69c7e3babea1df1a110903399b577cbc
leofrio <951549927@adm.unifor.br@DTEC65046812.unifor.br>
2022-12-20 15:09:43 -0300

    fixing solicitation filtering

--- src/main/java/com/CoffeControl/backend/repository/SolicitationRepository.java
+++ src/main/java/com/CoffeControl/backend/repository/SolicitationRepository.java
@@ -13,11 +13,12 @@ public interface SolicitationRepository extends JpaRepository<Solicitation,Integ
             " inner join solicitations s on s.id=sp.solicitation_id " +
             " where sp.solicitation_id = :solicitationId;",nativeQuery = true)
     public List<Integer> getRequiredAmounts(Integer solicitationId);
-    @Query(value = "select s from solicitation_product sp " +
+    @Query(value = "select s.* from solicitation_product sp " +
                 " inner join solicitations s on s.id = sp.solicitation_id " +
                 " inner join products p on p.id = sp.product_id " +
                 " inner join users u on u.id = s.assigned_user_id " +
                 " where(lower(s.solicitation_name) like concat('%', :name , '%') or :name  is null) and " +
+                " (lower(p.description) like concat('%', :description , '%') or :description is null) and" +
                 " (lower(u.full_name) like concat('%', :username ) or :username is null) and " +
                 " (s.enabled = :enabled or :enabled is null)  and " +
                 " (u.pword = :password or :password is null)and " +
